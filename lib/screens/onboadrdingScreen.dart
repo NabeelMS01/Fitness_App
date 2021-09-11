19a43103a32c729f8770c314bbfc6add1fe9ce51
Nabeel M.S <nabeel.ms78@gmail.com>
2021-09-11 14:29:11 +0530

    added some json file to learn http

--- lib/screens/onboadrdingScreen.dart
+++ lib/screens/onboadrdingScreen.dart
@@ -5,6 +5,8 @@
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
+import 'homeScreen.dart';
+
 class OnBoardingScreen extends StatefulWidget {
   const OnBoardingScreen({Key? key}) : super(key: key);
 
@@ -18,27 +20,53 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
       title: "Title of first page",
       body:
           "Here you can write the description of the page, to explain something...",
-      image: Center(child: Image.network("https://github.com/NabeelMS1997/assets/blob/main/screen1.png?raw=true", height: 200.0,width: 200,)),
+      image: Center(
+          child: Image.asset(
+        "assets/onboardingScreen/screen1.png",
+        height: 200.0,
+        width: 200,
+      )),
       decoration: const PageDecoration(
-        pageColor: Colors.blue,
+          pageColor: Color(0xFF002970),
+          bodyTextStyle: TextStyle(color: Colors.white, fontSize: 16),
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+          ),
+          fullScreen: false),
+    ),
+    PageViewModel(
+      title: "Title of first page",
+      body:
+          "Here you can write the description of the page, to explain something...",
+      image: Center(
+          child: Image.asset(
+        "assets/onboardingScreen/screen2.png",
+        height: 200.0,
+        width: 200,
+      )),
+      decoration: const PageDecoration(
+        pageColor: Color(0xFF002970),
+        titleTextStyle: TextStyle(color: Colors.white, fontSize: 24),
+        fullScreen: false,
+        bodyTextStyle: TextStyle(color: Colors.white, fontSize: 16),
       ),
     ),
     PageViewModel(
       title: "Title of first page",
       body:
           "Here you can write the description of the page, to explain something...",
-      image: Center(child: Image.network("https://github.com/NabeelMS1997/assets/blob/main/screen2.png?raw=true", height: 200.0,width: 200,)),
+      image: Center(
+          child: Image.asset(
+        "assets/onboardingScreen/screen3.png",
+        height: 200.0,
+        width: 200,
+      )),
       decoration: const PageDecoration(
-        pageColor: Colors.blue,
-      ),
-    ),
-    PageViewModel(
-      title: "Title of first page",
-      body:
-          "Here you can write the description of the page, to explain something...",
-      image: Center(child: Image.network("https://github.com/NabeelMS1997/assets/blob/main/screen3.png?raw=true", height: 200.0,width: 200,)),
-      decoration: const PageDecoration(
-        pageColor: Colors.blue,
+        pageColor: Color(0xFF002970),
+        titleTextStyle: TextStyle(color: Colors.white, fontSize: 24),
+        fullScreen: false,
+        bodyTextStyle: TextStyle(color: Colors.white, fontSize: 16),
       ),
     )
   ];
@@ -49,6 +77,8 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
         body: IntroductionScreen(
       pages: pages,
       onDone: () {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const HomeScreen()));
         // When done button is press
       },
       onSkip: () {
@@ -61,7 +91,7 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
       dotsDecorator: DotsDecorator(
           size: const Size.square(10.0),
           activeSize: const Size(20.0, 10.0),
-          activeColor: Colors.deepOrange,
+          activeColor: Colors.orange,
           color: Colors.black26,
           spacing: const EdgeInsets.symmetric(horizontal: 3.0),
           activeShape: RoundedRectangleBorder(
@@ -69,3 +99,29 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
     ));
   }
 }
+
+// IntroductionScreen(
+//
+// pages: pages,
+//
+// onDone: () {
+// // When done button is press
+// },
+// onSkip: () {
+// // You can also override onSkip callback
+// },
+// showSkipButton: true,
+// skip: const Icon(Icons.skip_next),
+// next: const Icon(Icons.arrow_right),
+// done: const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
+// dotsDecorator: DotsDecorator(
+// size: const Size.square(10.0),
+// activeSize: const Size(20.0, 10.0),
+// activeColor: Colors.deepOrange,
+//
+// color: Colors.black26,
+// spacing: const EdgeInsets.symmetric(horizontal: 3.0),
+// activeShape: RoundedRectangleBorder(
+// borderRadius: BorderRadius.circular(25.0))),
+// globalFooter: ,
+// )
